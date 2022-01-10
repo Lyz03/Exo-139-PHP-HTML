acb80e2e09f6dd4469c0ee0dee993d153c5b5499
Lyz03 <lizoe@netcourrier.com>
2022-01-10 09:42:07 +0100

    part 2

--- index2.php
+++ index2.php
@@ -7,6 +7,11 @@
        <select  id="day">
            <?php
            // TODO list of day
+           $days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+           foreach ($days as $value) {
+               echo "<option>$value</option>";
+           }
            ?>
        </select>
 
@@ -14,6 +19,11 @@
        <select  id="month">
            <?php
            // TODO list of month
+           $months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+           foreach ($months as $value) {
+               echo "<option>$value</option>";
+           }
            ?>
        </select>
 
@@ -21,9 +31,11 @@
        <select  id="year">
            <?php
            // TODO list of year 2018, 2019, 2020 --> Bonus : faites le avec une vraie date en PHP, renseignez vous sur la doc
+           $years = [2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025];
 
+           foreach ($years as $value) {
+               echo "<option>$value</option>";
+           }
            ?>
        </select>
      </form>
-  </body>
-</html>
